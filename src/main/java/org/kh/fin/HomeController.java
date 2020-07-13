c9a49ed8f39eb8d8b3c41c52ef4d508cc1741548
NamNi-Kim <knh2432@gmail.com>
2020-07-13 16:25:13 +0900

    Revert "POM.XML"
    This reverts commit 8ae2c5cf2d601f0042d293d84721ab985a212c84.

--- src/main/java/org/kh/fin/HomeController.java
+++ src/main/java/org/kh/fin/HomeController.java
@@ -1,25 +1,15 @@
 package org.kh.fin;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.URL;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
@@ -29,97 +19,21 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@RequestMapping(value="map.do")
-	public String mapPage() {
-		return "common/map";
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
 	}
 	
-	@ResponseBody
-	@RequestMapping(value="search.do")
-	public void searchData(HttpServletRequest request, HttpServletResponse response){
-		try {
-			PrintWriter out = response.getWriter();
-			JSONObject jObject = new JSONObject();
-			response.setContentType("application/json");
-			response.setCharacterEncoding("utf-8");
-			
-			
-			//place 정보 추출
-			String url = request.getParameter("url").replace("http", "https");
-			url = url.replace("com/", "com/main/v/");
-			URL infoURL = new URL(url);
-	        
-			//URL infoURL = new URL("https://place.map.kakao.com/main/v/9575956");
-	        InputStream inputStream = infoURL.openStream();
-	        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-
-	        StringBuffer sb = new StringBuffer();
-	        int readByte;
-	        while ((readByte = inputStreamReader.read()) != -1) {
-	            sb.append((char) readByte);
-	        }
-	        inputStreamReader.close();
-	        inputStream.close();
-	        
-	        JSONParser parser = new JSONParser();
-	        Object obj = parser.parse(sb.toString());
-	        JSONObject jsonObj = (JSONObject) obj;                
-	        jObject.put("placeData", jsonObj);
-	        
-	        //weather 정보 추출
-	        
-	        url 	= "http://api.openweathermap.org/data/2.5/weather?lat="
-					+ request.getParameter("lat")+"&lon="
-					+ request.getParameter("lon")+"&units=metric&appid=3321c6e817ec067d4099feef829b4fb7";
-			
-	        infoURL = new URL(url);
-			
-	        inputStream = infoURL.openStream();
-	        inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-
-	        sb = new StringBuffer();
-	        
-	        int readByte2;
-	        while ((readByte2 = inputStreamReader.read()) != -1) {
-	            sb.append((char) readByte2);
-	        }
-	        inputStreamReader.close();
-	        inputStream.close();
-	                
-	        obj = parser.parse(sb.toString());
-	        jsonObj = (JSONObject) obj;  
-	        
-	        jObject.put("weatherData", jsonObj);
-	        
-	        //weather Daily 정보 추출
-	        url 	= "https://api.openweathermap.org/data/2.5/onecall?lat="
-					+ request.getParameter("lat")+"&lon="
-					+ request.getParameter("lon")+"&units=metric&%20exclude=daily&appid=3321c6e817ec067d4099feef829b4fb7";
-			
-	        infoURL = new URL(url);
-			
-	        inputStream = infoURL.openStream();
-	        inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-
-	        sb = new StringBuffer();
-	        
-	        int readByte3;
-	        while ((readByte3 = inputStreamReader.read()) != -1) {
-	            sb.append((char) readByte3);
-	        }
-	        inputStreamReader.close();
-	        inputStream.close();
-	                
-	        obj = parser.parse(sb.toString());
-	        jsonObj = (JSONObject) obj;  
-	        
-	        jObject.put("weatherList", jsonObj);
-	        
-	        out.print(jObject);
-	    	out.flush();
-			out.close();
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	}
 }
